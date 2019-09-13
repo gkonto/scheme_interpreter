27bfea1e17ada18857f8dc5475f9470ab7fb4da7
giorgos <gkonto>
2019-09-13 22:45:00 +0300

    Added type identifier functions to class Node. eg is_boolean(), is_fixnum() etc

--- node.hpp
+++ node.hpp
@@ -5,13 +5,26 @@
 //This is abstract
 class Node
 {
-
+	public: 
+		virtual bool is_symbol()         { return false; }
+		virtual bool is_pair()           { return false; }
+		virtual bool is_boolean()        { return false; }
+		virtual bool is_the_empty_list() { return false; }
+		virtual bool is_fixnum()         { return false; }
+		virtual bool is_character()      { return false; }
+		virtual bool is_string()         { return false; }
+		virtual bool is_primitive_proc() { return false; }
+		virtual bool is_compound_proc()  { return false; }
+		virtual bool is_input_port()     { return false; }
+		virtual bool is_output_port()    { return false; }
+		virtual bool is_eof()            { return false; } 
 };
 
 class Fixnum : public Node
 {
 	public:
 		explicit Fixnum(long val) : value_(val) {}
+		virtual bool is_fixnum() { return true; }
 	private:
 		long value_;
 };
@@ -20,6 +33,7 @@ class Boolean : public Node
 {
 	public:
 		explicit Boolean(bool val) : value_(val) {}
+		bool is_boolean() { return true; }
 	private:
 		bool value_;
 };
@@ -28,6 +42,7 @@ class Char : public Node
 {
 	public:
 		explicit Char(char val) : value_(val) {}
+		bool is_character() { return true; }
 	private:
 		char value_;
 };
@@ -36,6 +51,7 @@ class String : public Node
 {
 	public:
 		explicit String(const std::string &val) : value_(val) {}
+		bool is_string() { return true; }
 	private:
 		std::string value_;
 };
@@ -44,6 +60,7 @@ class EmptyList : public Node
 {
 	public:
 		explicit EmptyList() {}
+		bool is_the_empty_list() { return true; }
 	private:
 };
 
@@ -51,6 +68,7 @@ class Symbol : public Node
 {
 	public:
 		explicit Symbol(const std::string &label) : label_(label) {} 
+		bool is_symbol() { return true; }
 	private:
 		const std::string label_;
 
@@ -60,6 +78,7 @@ class Pair : public Node
 {
 	public:
 		explicit Pair(Node *car, Node *cdr) : car_(car), cdr_(cdr) {}
+		bool is_pair() { return true; }
 	private:
 		Node *car_;
 		Node *cdr_;
@@ -69,8 +88,8 @@ class PrimitiveProc : public Node
 {
 	public:
 		using PrimitiveProcFun = Node *(*)(Node *args);
-
 		explicit PrimitiveProc(PrimitiveProcFun fun) : fun_(fun) {}
+		bool is_primitive_proc() { return true; }
 	private:
 		PrimitiveProcFun fun_;
 };
@@ -79,9 +98,10 @@ class PrimitiveProc : public Node
 class CompoundProc : public Node
 {
 	public:
-		CompoundProc(Node *params, Node *body, Node *env) : 
+		explicit CompoundProc(Node *params, Node *body, Node *env) : 
        			parameters_(params), body_(body), env_(env)
 			{}
+		bool is_compound_proc() { return true; }
 	private:
 		Node *parameters_;
 		Node *body_;
@@ -92,7 +112,8 @@ class CompoundProc : public Node
 class InputPort : public Node
 {
 	public:
-		InputPort(FILE *stream) : stream_(stream) {}
+		explicit InputPort(FILE *stream) : stream_(stream) {}
+		bool is_input_port() { return true; }
 	private:
 		FILE *stream_;//TODO replace FILE * with c++
 };
@@ -100,7 +121,8 @@ class InputPort : public Node
 class OutputPort : public Node
 {
 	public:
-		OutputPort(FILE *stream) : stream_(stream) {}
+		explicit OutputPort(FILE *stream) : stream_(stream) {}
+		bool is_output_port() { return true; }
 	private:
 		FILE *stream_;
 };
@@ -109,6 +131,7 @@ class Eof
 {
 	public:
 		Eof() {}
+		bool is_eof() { return true; }
 	private:
 };
 
